1db0593b59a04e63eb640159405e0c712a9bca57
syazaniskywalker <syazani@9loop.asia>
2024-01-17 18:53:27 +0800

    🧹:

--- src/lib/sovereignty_/colosseum.ts
+++ src/lib/sovereignty_/colosseum.ts
@@ -2,18 +2,28 @@ import yaml from 'js-yaml';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { _path } from './_utils';
-import type { Player } from './player';
+import { Player } from './player';
 // __luggage
 
 export class Colosseum {
 	// const players: Player[] = []
 
-	constructor(_players: string) {
-		const shine = _path('src', 'lib', 'lobby', _players)
-		let list = this.colosseumOpens(shine);
+	constructor(playerList: string) {
+		const light_shines_on_the_players = _path('src', 'lib', 'lobby', playerList)
+		let usernames = this.colosseumOpens(light_shines_on_the_players);
+
+		const x = this.gatherThePlayers(usernames);
 	}
 
-    // __luggage
+	private gatherThePlayers(usernames: string[]) {
+		for (const username of usernames) {
+			const y = _path('src', 'lib', 'lobby', '_players', username + '.ts')
+			const x = readFileSync(y, 'utf8')
+			console.log(x)
+		}
+	}
+
+	// __luggage
 	private colosseumOpens(_players: string): string[] {
 		return yaml.load(readFileSync(_players, 'utf8')) as string[];
 	}
