94f828fc40259e7ad480220ccc794cd1622e819a
syazsolo <syazani@9loop.asia>
2024-03-16 02:44:47 +0800

    :construction:

--- duel.ts
+++ duel.ts
@@ -1,3 +1,21 @@
-// whos player 1
-// whos player 2
-// start dueling?
+import { Decision, Player } from "./types";
+import { log } from "./utils";
+
+function duel(player1: Player, player2: Player, totalTurns: number = 50) {
+    runTurn(player1, player2, 1);
+    runTurn(player1, player2, 1);
+    runTurn(player1, player2, 1);
+}
+
+function runTurn(player1: Player, player2: Player, turn: number) {
+    const decision1 = player1.decide(turn);
+    const decision2 = player2.decide(turn);
+
+    // pass decision
+
+    // count points
+}
+
+function inform(player: Player, decision: Decision) {
+    // do what
+}
---
+++ types.ts
@@ -0,0 +1,5 @@
+export type Player = {
+    decide: (turn: number) => Decision,
+}
+
+export type Decision = {}
