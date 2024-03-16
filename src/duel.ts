4b64911d244e9c918a216d43097ca917a1e89e96
syazsolo <syazani@9loop.asia>
2024-03-16 13:23:35 +0800

    :construction:

--- src/duel.ts
+++ src/duel.ts
@@ -5,55 +5,58 @@ export class Duel {
     #pp1: PlayerPoints;
     #pp2: PlayerPoints;
 
-    constructor(player1: Player, player2: Player, totalTurns: number = 50) {
+    constructor(player1: Player, player2: Player) {
         this.#pp1 = new PlayerPoints(player1);
         this.#pp2 = new PlayerPoints(player2);
     }
 
     run(totalTurns: number = 50): DuelReport {
         for (let i = 1; i < totalTurns; i++) {
-            const { p1Points, p2Points } = run_turn(this.#pp1.player, this.#pp2.player);
+            const { p1Points: p1_points, p2Points: p2_points } = this.#runTurn();
 
-            this.#pp1.rewardPoints(p1Points);
-            this.#pp2.rewardPoints(p2Points);
+            this.#pp1.rewardPoints(p1_points);
+            this.#pp2.rewardPoints(p2_points);
         }
 
-        const winner = (() => {
-            const p1_points = this.#pp1.points;
-            const p2_points = this.#pp2.points;
-
-            if (p1_points === p2_points) {
-                return '-';
-            }
-            if (p1_points > p2_points) {
-                return this.#pp1.player.name;
-            }
-            return this.#pp2.player.name
-        })();
-
         return {
-            winner,
+            winner: this.#winner(),
             totalTurns,
             player1: this.#pp1.report(),
             player2: this.#pp2.report()
         }
     }
-}
 
-function run_turn(player1: Player, player2: Player): TurnReturns {
-    // both players make the decision
-    const p1_decision = player1.decide();
-    const p2_decision = player2.decide();
+    #runTurn(): TurnReturns {
+        const player1 = this.#pp1.player;
+        const player2 = this.#pp2.player;
 
-    // both players receive the decision
-    player1.receives(p2_decision);
-    player2.receives(p1_decision);
+        // both players make the decision
+        const p1_decision = player1.decide();
+        const p2_decision = player2.decide();
+    
+        // both players receive the decision
+        player1.receives(p2_decision);
+        player2.receives(p1_decision);
+    
+        // calculate the points
+        const p1_points = calculate_points(p1_decision, p2_decision);
+        const p2_points = calculate_points(p2_decision, p1_decision);
+    
+        return { p1Points: p1_points, p2Points: p2_points }
+    }
 
-    // calculate the points
-    const p1_points = calculate_points(p1_decision, p2_decision);
-    const p2_points = calculate_points(p2_decision, p1_decision);
+    #winner() {
+        const p1_points = this.#pp1.points;
+        const p2_points = this.#pp2.points;
 
-    return { p1Points: p1_points, p2Points: p2_points }
+        if (p1_points === p2_points) {
+            return '-';
+        }
+        if (p1_points > p2_points) {
+            return this.#pp1.player.name;
+        }
+        return this.#pp2.player.name
+    }
 }
 
 function calculate_points(gives: Decision, receives: Decision): number {
--- src/player.ts
+++ src/player.ts
@@ -2,24 +2,17 @@ import { Decision } from './types';
 
 export class Player {
     name: string;
-    #currentTurn: number = 1;
-    #opponentHistory: Decision[];
+    #receives: Decision[] = [];
 
     constructor(name: string) {
         this.name = name;
     }
 
     decide(): Decision {
-        this.#currentTurn += 1;
-
         return 'cooperate';
     }
 
     receives(decision: Decision): void {
-        this.#opponentHistory.push(decision);
-    }
-
-    totalTurns(): number {
-        return this.#currentTurn - 1;
+        this.#receives.push(decision);
     }
 }
